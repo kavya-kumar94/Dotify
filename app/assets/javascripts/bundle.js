26c5d3d3fe5479e871631d854b4db333d495f7df
Kavya Kumar <tellkavya@gmail.com>
2019-07-08 18:23:33 -0700

    Add player icons

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -1189,7 +1189,37 @@ function (_React$Component) {
     value: function render() {
       return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "player-div"
-      });
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "left-play"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+        className: "love",
+        src: "https://dotify-app-dev.s3-us-west-1.amazonaws.com/love_empty.png"
+      })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "center-play"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+        className: "repeat",
+        src: "https://dotify-app-dev.s3-us-west-1.amazonaws.com/repeat_grey.png"
+      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+        className: "prev",
+        src: "https://dotify-app-dev.s3-us-west-1.amazonaws.com/prev_grey.png"
+      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+        className: "play",
+        src: "https://dotify-app-dev.s3-us-west-1.amazonaws.com/play_grey.png"
+      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+        className: "next",
+        src: "https://dotify-app-dev.s3-us-west-1.amazonaws.com/next_grey.png"
+      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+        className: "shuffle",
+        src: "https://dotify-app-dev.s3-us-west-1.amazonaws.com/shuffle_grey.png"
+      })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "right-play"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+        className: "playlist",
+        src: "https://dotify-app-dev.s3-us-west-1.amazonaws.com/playlist-grey.png"
+      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+        className: "volume",
+        src: "https://dotify-app-dev.s3-us-west-1.amazonaws.com/volume_grey.png"
+      })));
     }
   }]);
 
--- frontend/components/music_player/player.jsx
+++ frontend/components/music_player/player.jsx
@@ -8,7 +8,20 @@ class Player extends React.Component{
     render() {
         return (
             <div className="player-div">
-                
+                <div className="left-play">
+                    <img className="love" src="https://dotify-app-dev.s3-us-west-1.amazonaws.com/love_empty.png"/>
+                </div>
+                <div className="center-play">
+                    <img className="repeat" src="https://dotify-app-dev.s3-us-west-1.amazonaws.com/repeat_grey.png"/>
+                    <img className="prev" src="https://dotify-app-dev.s3-us-west-1.amazonaws.com/prev_grey.png"/>
+                    <img className="play" src="https://dotify-app-dev.s3-us-west-1.amazonaws.com/play_grey.png"/>
+                    <img className="next" src="https://dotify-app-dev.s3-us-west-1.amazonaws.com/next_grey.png"/>
+                    <img className="shuffle" src="https://dotify-app-dev.s3-us-west-1.amazonaws.com/shuffle_grey.png"/>
+                </div>
+                <div className="right-play">
+                    <img className="playlist" src="https://dotify-app-dev.s3-us-west-1.amazonaws.com/playlist-grey.png"/>
+                    <img className="volume" src="https://dotify-app-dev.s3-us-west-1.amazonaws.com/volume_grey.png"/>
+                </div>
             </div>
         )
     }
