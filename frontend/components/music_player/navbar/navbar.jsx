19f8d9ac5c4beb5ffab1147006005b2fcbaa49a2
Kavya Kumar <tellkavya@gmail.com>
2019-08-16 18:48:56 -0700

    Add styling

--- frontend/components/music_player/navbar/navbar.jsx
+++ frontend/components/music_player/navbar/navbar.jsx
@@ -36,7 +36,7 @@ class Navbar extends React.Component {
         let playlistsNew = playlists.map((playlist, idx) => {
             return (
                 <div key={idx} className="">
-                    <li className=""><NavLink to={`/playlists/${playlist.id}`}>{playlist.title}</NavLink></li>
+                    <li activeClassName="newactive"><NavLink to={`/playlists/${playlist.id}`}>{playlist.title}</NavLink></li>
                 </div>
             )
         })
@@ -53,7 +53,7 @@ class Navbar extends React.Component {
                                 </div>
                             
                             <li>
-                                <NavLink className="not-active" activeClassName="active" to='/browse/featured' >
+                                <NavLink className="inactive" activeClassName="active" to='/browse/featured' >
                                     <div className="navbar-text">
                                         <svg viewBox="0 0 512 512" width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M 256.274 60.84 L 84.324 166.237 L 84.324 443.063 L 193.27 443.063 L 193.27 293.73 L 320.228 293.73 L 320.228 443.063 L 428.222 443.063 L 428.222 165.476 L 256.274 60.84 Z M 256.274 35.95 L 448.452 149.145 L 448.452 464.395 L 300 464.395 L 300 315.062 L 213.499 315.062 L 213.499 464.395 L 64.095 464.395 L 64.095 150.161 L 256.274 35.95 Z" fill="currentColor"></path></svg>
                                         {/* <img className="home" src="https://dotify-app-dev.s3-us-west-1.amazonaws.com/home-logo.png" /> */}
@@ -62,7 +62,7 @@ class Navbar extends React.Component {
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink className="not-active" activeClassName="active" to='/search' >
+                                <NavLink className="inactive" activeClassName="active" to='/search' >
                                     <div className="navbar-text">
                                         <svg viewBox="0 0 512 512" width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M349.714 347.937l93.714 109.969-16.254 13.969-93.969-109.969q-48.508 36.825-109.207 36.825-36.826 0-70.476-14.349t-57.905-38.603-38.603-57.905-14.349-70.476 14.349-70.476 38.603-57.905 57.905-38.603 70.476-14.349 70.476 14.349 57.905 38.603 38.603 57.905 14.349 70.476q0 37.841-14.73 71.619t-40.889 58.921zM224 377.397q43.428 0 80.254-21.461t58.286-58.286 21.461-80.254-21.461-80.254-58.286-58.285-80.254-21.46-80.254 21.46-58.285 58.285-21.46 80.254 21.46 80.254 58.285 58.286 80.254 21.461z" fill="currentColor" ></path></svg>
                                         {/* <img className="search" src="https://dotify-app-dev.s3-us-west-1.amazonaws.com/search-logo.png"/> */}
@@ -71,7 +71,7 @@ class Navbar extends React.Component {
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink className="not-active" activeClassName="active" to='/library/playlists' >
+                                <NavLink className="inactive" activeClassName="active" to='/library/playlists' >
                                     <div className="navbar-text navbar-library">
                                         <svg viewBox="0 0 512 512" width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M291.301 81.778l166.349 373.587-19.301 8.635-166.349-373.587zM64 463.746v-384h21.334v384h-21.334zM192 463.746v-384h21.334v384h-21.334z" fill="currentColor"></path></svg>
                                     {/* <i className="nav-icon spoticon-collection-16"></i> */}
