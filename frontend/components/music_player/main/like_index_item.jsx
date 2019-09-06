7201300fb7d49f45eaf88a7913766b237e20ffa5
Kavya Kumar <tellkavya@gmail.com>
2019-09-06 10:50:35 -0700

    Add LikeIndexItem

---
+++ frontend/components/music_player/main/like_index_item.jsx
@@ -0,0 +1,140 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { openModal } from '../../../actions/modal_actions';
+import { connect } from 'react-redux'
+import { setCurrentSong, setQueue, toggleSong, addToQueue } from '../../../actions/player_actions';
+import { receiveSongId } from '../../../actions/song_actions'
+
+class LikeIndexItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            noteIcon: "https://dotify-app-dev.s3-us-west-1.amazonaws.com/music_note.png",
+            noteClass: "art-song",
+            addIcon: "https://dotify-app-dev.s3-us-west-1.amazonaws.com/handtinytrans.gif",
+            playing: false,
+            currentSong: this.props.currentSong
+        }
+        this.note = this.note.bind(this);
+        this.play = this.play.bind(this);
+        this.song = this.song.bind(this);
+        this.songAdd = this.songAdd.bind(this);
+
+    }
+
+    componentDidMount() {
+        let audio = document.querySelector('#audio');
+        if (audio) {
+            this.setState({ currentSong: this.props.currentSong });
+        }
+    }
+
+    note() {
+        this.setState({
+            noteIcon: "https://dotify-app-dev.s3-us-west-1.amazonaws.com/music_note.png",
+            noteClass: "art-song",
+            addIcon: "https://dotify-app-dev.s3-us-west-1.amazonaws.com/handtinytrans.gif"
+
+        })
+    }
+
+    play() {
+        this.setState({
+            noteIcon: "https://dotify-app-dev.s3-us-west-1.amazonaws.com/play_white.png",
+            noteClass: "art-song2",
+            addIcon: "https://dotify-app-dev.s3-us-west-1.amazonaws.com/3dots.png"
+
+        })
+    }
+
+    song() {
+        let audio = document.getElementById('audio');
+        console.log(this.props.playing);
+
+        if (this.props.playing === false) {
+            var playPromise = audio.play();
+            if (playPromise !== undefined) {
+                playPromise.then(() => {
+                    // Automatic playback started!
+                    // Show playing UI.
+                    console.log("done")
+                })
+                    .catch(error => {
+                        // Auto-play was prevented
+                        // Show paused UI.
+                    });
+            }
+            // audio.play();
+            this.props.setCurrentSong(this.props.like);
+            this.props.toggleSong();
+            this.setState({
+                playing: true,
+                // play: "https://dotify-app-dev.s3-us-west-1.amazonaws.com/pause_grey.png"
+            })
+        } else if (this.props.playing === true) {
+            this.props.setCurrentSong(this.props.like);
+            audio.pause();
+            this.props.toggleSong();
+            this.setState({
+                playing: false,
+                // play: "https://dotify-app-dev.s3-us-west-1.amazonaws.com/play_grey.png"
+            })
+        }
+    }
+
+    songAdd(songId) {
+        this.props.openModal("add-song");
+        this.props.receiveSongId(songId);
+    }
+
+    render() {
+        let noteClass = this.state.noteClass;
+        const { like } = this.props;
+        let { openModal } = this.props
+
+        return (
+            <div onMouseEnter={this.play} onMouseLeave={this.note} className={noteClass}>
+                <div className="flex">
+                    <div>
+                        <img onClick={() => this.song()} id="art-note" src={this.state.noteIcon} />
+                    </div>
+                    <div className="song-info">
+                        <li>{like.title}</li>
+                        <div className="alb-art">
+                            <li><span><NavLink to={`/albums/${like.album_id}`}>{like.album_title}</NavLink></span></li>
+                            <li className="sep">•</li>
+                            <li><span><NavLink to={`/artists/${like.artist_id}`}>{like.artist_name}</NavLink></span></li>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="add-duration">
+                    <img onClick={() => this.songAdd(like.id)} id="add-song-menu" src={this.state.addIcon} />
+                    {like.duration}
+                </div>
+
+            </div>
+        )
+    }
+}
+
+const msp = state => {
+    return {
+        currentSong: state.ui.playStatus.currentSong,
+        playing: state.ui.playStatus.playing,
+        // songId: state.entities.addSong.songId
+    }
+}
+
+const mdp = dispatch => {
+    return {
+        openModal: (modal) => dispatch(openModal(modal)),
+        setCurrentSong: (song) => (dispatch(setCurrentSong(song))),
+        toggleSong: () => (dispatch(toggleSong())),
+        setQueue: (queue) => (dispatch(setQueue(queue))),
+        receiveSongId: (songId) => dispatch(receiveSongId(songId))
+    }
+}
+
+
+export default connect(msp, mdp)(LikeIndexItem);
--- frontend/components/music_player/main/likes_index.jsx
+++ frontend/components/music_player/main/likes_index.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { NavLink, Link } from 'react-router-dom';
+import { clearPlaylistSongs } from '../../../actions/song_actions'
 import { fetchLikedSongs, saveSong, unsaveSong } from '../../../actions/like_actions';
-
+import LikeIndexItem from './like_index_item'
 class LikesIndex extends React.Component {
     constructor(props) {
         super(props);
     }
 
     componentDidMount() {
+        this.props.clearPlaylistSongs();
         this.props.fetchLikedSongs();
     }
      
     render() {
         let {likes} = this.props
         return (
-            <ul>
-                {likes.map(like => {
-                    return like.title
+            <div className="track6">
+                {likes.map((like, idx) => {
+                    return <LikeIndexItem key={idx} like={like} />
                 })}
-            </ul>
+            </div>
+               
         )
     }
 }
@@ -34,6 +37,7 @@ export const msp = state => {
 export const mdp = dispatch => {
     return {
         fetchLikedSongs: () => dispatch(fetchLikedSongs()),
+        clearPlaylistSongs: () => dispatch(clearPlaylistSongs()),
         saveSong: (userId, songId) => dispatch(saveSong(userId, songId)),
         unsaveSong: (likedSongId) => dispatch(unsaveSong(likedSongId))
     }
